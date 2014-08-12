fcf07e282aa4fef7bb96e43409fd59531cd941b1
Max Holman <max@holmn.com>
2014-08-12 12:31:58 +0800

    First working commit

--- umd.js
+++ umd.js
@@ -1,10 +1,14 @@
-if (typeof module === 'object' && typeof define !== 'function') {
-    var define = function (factory) {
-        module.exports = factory(require, exports, module);
-    };
-}
-
-define(function (require, exports, module) {
-    var i18n = require('messageformat');
-    return null;
-});
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['exports'], factory);
+    } else if (typeof exports === 'object') {
+        // CommonJS
+        factory(exports);
+    } else {
+        // Browser globals
+        factory((root.commonJsStrict = {}));
+    }
+}(this, function (exports) {
+    return;
+}));
