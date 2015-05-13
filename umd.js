33c4675f015a20f954040eb36825dee78bc92b8f
Max Holman <max@holmn.com>
2015-05-13 11:22:32 +0800

    use a more appropriate UMD

--- umd.js
+++ umd.js
@@ -1,14 +1,21 @@
+// if the module has no dependencies, the above pattern can be simplified to
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(['exports'], factory);
+        define([], factory);
     } else if (typeof exports === 'object') {
-        // CommonJS
-        factory(exports);
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
     } else {
-        // Browser globals
-        factory((root.commonJsStrict = {}));
+        // Browser globals (root is window)
+        root.returnExports = factory();
     }
-}(this, function (exports) {
+}(this, function () {
+
+    // Just return a value to define the module export.
+    // This example returns an object, but the module
+    // can return a function as the exported value.
     return;
 }));
